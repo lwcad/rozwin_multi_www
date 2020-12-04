40513ffeb767d9adae2b63613ab6f545993e43cd
lwcad <33753788+lwcad@users.noreply.github.com>
2020-12-04 12:49:57 +0100

    Add files via upload
    + classes o get data numbers

--- rozwin_multi.py
+++ rozwin_multi.py
@@ -1,4 +1,72 @@
 import tkinter as tk
+from tkinter import messagebox
+
+
+
+class dana_wiersz:
+
+    def __init__(self, ramka, wiersz, nazwa, wartosc, jednostka):
+        self.ramka  = ramka
+        self.wiersz = wiersz
+        self.nazwa  = nazwa
+        self.wartosc = wartosc
+        self.jednostka = jednostka
+        self.lbl_nazwa     = tk.Label (master=self.ramka, text=self.nazwa)
+        self.edt_wartosc   = tk.Entry (master=self.ramka, width=10)
+        self.lbl_jednostka = tk.Label (master=self.ramka, text=self.jednostka)
+
+
+    def wyswietl(self):
+
+        self.lbl_nazwa.grid     (row=self.wiersz, column=0, sticky="w", padx=5, pady=1)        
+        self.edt_wartosc.grid   (row=self.wiersz, column=1, sticky="w")
+        self.lbl_jednostka.grid (row=self.wiersz, column=2, sticky="w", padx=5)
+       
+        self.edt_wartosc.insert(0, self.wartosc) # czy to coś zmienia np. zmienną wartość
+
+
+    def zwroc_dana(self):
+#        tak też działa
+#        self.wartosc = self.edt_wartosc.get()
+#        return self.wartosc
+         return self.edt_wartosc.get() 
+
+class dana_wiersz_liczb_real(dana_wiersz):
+    # przysłaniam metodę
+    def zwroc_dana(self):
+        a = self.edt_wartosc.get()
+        
+        # wyjątek
+        try:
+            a = float(a)
+            #messagebox.showinfo(title="Info", message="To jest liczba FLOAT : " + str(a))
+        except ValueError as e:
+            messagebox.showinfo(title="Wyjatek", message="To NIE jest liczba FLOAT : " + str(a))
+        finally:
+            lbl_wybor["text"] = "Zwalniam zasób"
+    
+        a = float(a)
+
+        return a
+    
+
+class dana_wiersz_liczb_int(dana_wiersz):
+    # przysłaniam metodę
+    def zwroc_dana(self):
+        a = self.edt_wartosc.get()
+        
+        # wyjątek
+        try:
+            a = int(a)
+            #messagebox.showinfo(title="Info", message="To jest liczba INT : " + str(a))
+        except ValueError as e:
+            messagebox.showinfo(title="Wyjatek", message="To NIE jest liczba INT : " + str(a))
+        finally:
+            lbl_wybor["text"] = "Zwalniam zasób"
+
+        a = int(a)
+
+        return a
 
 
 def sel():
@@ -7,6 +75,7 @@ def sel():
     lbl_wybor["text"] = selection
     # oba powyższe postawienia pod etykietę działają
 
+# -------------- Obsługa przycisków Wyników ----------------------------------------------    
 def zapisz_wyniki_txt():
     lbl_wybor["text"] = "Wyniki do pliku TXT"
 
@@ -18,8 +87,26 @@ def drukuj_wyniki():
 
 def zobacz_rysunek():
     lbl_wybor["text"] = "Zobacz Rysunek"
+
+# -------------- Obliczenia rozwinięcia ----------------------------------------------    
+def licz():
+    tmp_lancuch = ""
+    tmp1 = ""
+    arr_obj_dane = [ dana_Dm, dana_Dd, dana_P, dana_Beta, dana_Alfa, dana_H, dana_Ld, dana_Lm, dana_N ]
     
-    
+    for x in arr_obj_dane:
+        tmp1 = x.zwroc_dana()
+        if ( (type( tmp1 ) == float) or (type( tmp1 ) == int) ) :
+            tmp_lancuch = tmp_lancuch + " " + str(tmp1) + "flin"
+        elif  (type( tmp1 ) == str):
+            tmp_lancuch = tmp_lancuch + " " + tmp1 + "str"
+        else:
+            tmp_lancuch = tmp_lancuch + "Typ: inny" 
+        
+    lbl_wybor["text"] = tmp_lancuch
+
+# -------------- KONIEC - Obliczenia rozwinięcia ----------------------------------------------
+
 window = tk.Tk()
 window.title("Rozwinięcia 2")
 
@@ -71,23 +158,47 @@ lbl_wybor.grid(row=2, column=0, sticky="w")
 #-- Koniec podramki frame_dane_a w Ramce fr_dane
 
 #-- Podramka fr_dane_b w Ramce fr_dane - zawartość DANE
-# Dm
-lbl_Dm = tk.Label      (master=fr_dane_b, text="Dm =")
-ent_Dm = tk.Entry      (master=fr_dane_b, width=10)
-lbl_Dm_jedn = tk.Label (master=fr_dane_b, text="[mm]")
 
-lbl_Dm.grid(row=0, column=0, sticky="w", padx=5, pady=5)
-ent_Dm.grid(row=0, column=1, sticky="w")
-lbl_Dm_jedn.grid(row=0, column=2, sticky="w", padx=5)
+# Dm
+dana_Dm = dana_wiersz_liczb_real  ( fr_dane_b, 0 , "Dm =", "300", "[mm]")
+dana_Dm.wyswietl()
+#lbl_wybor["text"] = dana_Dm.zwroc_dana()
 
 # Dd
-lbl_Dd = tk.Label      (master=fr_dane_b, text="Dd =")
-ent_Dd = tk.Entry      (master=fr_dane_b, width=10)
-lbl_Dd_jedn = tk.Label (master=fr_dane_b, text="[mm]")
+dana_Dd = dana_wiersz_liczb_real   ( fr_dane_b, 1 , "Dd =", "800", "[mm]")
+dana_Dd.wyswietl()
 
-lbl_Dd.grid(row=1, column=0, sticky="e", padx=5, pady=5)
-ent_Dd.grid(row=1, column=1, sticky="w")
-lbl_Dd_jedn.grid(row=1, column=2, sticky="w", padx=5)
+# P
+dana_P = dana_wiersz_liczb_real    ( fr_dane_b, 2 , "P =", "200", "[mm]")
+dana_P.wyswietl()
+
+#Beta
+dana_Beta = dana_wiersz_liczb_real ( fr_dane_b, 3 , "BETA =", "90", "["+chr(176)+"]")
+dana_Beta.wyswietl()
+
+#Alfa
+dana_Alfa = dana_wiersz_liczb_real ( fr_dane_b, 4 , "alfa =", "0", "["+chr(176)+"]")
+dana_Alfa.wyswietl()
+
+# H
+dana_H = dana_wiersz_liczb_real    ( fr_dane_b, 5 , "H =", "700", "[mm]")
+dana_H.wyswietl()
+
+# Ld
+dana_Ld = dana_wiersz_liczb_real   ( fr_dane_b, 6 , "Ld =", "1000", "[mm]")
+dana_Ld.wyswietl()
+
+# Lm
+dana_Lm = dana_wiersz_liczb_real   ( fr_dane_b, 7 , "Lm =", "350", "[mm]")
+dana_Lm.wyswietl()
+
+# Lm
+dana_N = dana_wiersz_liczb_int     ( fr_dane_b, 8 , "N =", "24", "[-]")
+dana_N.wyswietl()
+
+# Button - przycisk LICZ
+btn_licz  = tk.Button(fr_dane_b, text="Licz", width=10, height=1, command= licz )
+btn_licz.grid  (row=8, column=3, sticky="nsew", padx = 10 )
 
 #-- Koniec podramki frame_dane_b w Ramce fr_dane
 
@@ -179,3 +290,20 @@ if __name__ == "__main__" :
   g  := StrToFloat(rurarura.Edit_g.Text) ;
   ro := StrToFloat(rurarura.Edit_ro.Text) ;
 """  
+# Próby
+"""
+dana_probna = dana_wiersz  ( fr_dane_b, 2 , "Dana probna jakas 1 =", "0.123", "[mm]")
+dana_probna.wyswietl()
+dana_probna2 = dana_wiersz ( fr_obrazek, 0 , "Dana probna jakas 2 =", "321,04163", "[mm]")
+dana_probna2.wyswietl()
+lbl_wybor["text"] = dana_probna2.zwroc_dana()
+"""
+
+
+
+"""
+    lbl_wybor["text"] = dana_Dm.zwroc_dana() + " " + dana_Dd.zwroc_dana() + " " + dana_P.zwroc_dana() + " " + \
+                        dana_Beta.zwroc_dana() + " " + dana_Alfa.zwroc_dana() + " " + dana_H.zwroc_dana() + " " + \
+                        dana_Ld.zwroc_dana() + " " + dana_Lm.zwroc_dana() + " " + dana_N.zwroc_dana()
+"""
+#    lbl_wybor["text"] = dana_Dm.edt_wartosc.get() +"  " + dana_Dd.edt_wartosc.get() + " " + dana_P.edt_wartosc.get()
